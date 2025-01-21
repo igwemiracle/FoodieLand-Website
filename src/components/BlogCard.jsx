ff44af732f103b9c8d16d943e08ea2b495dfbbef
igwemiracle <igwemiracle35@gmail.com>
2025-01-21 11:00:33 +0100

    foodieland

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -6,8 +6,8 @@ const BlogCard = ({ blog }) => {
       <img className="rounded-[20px] object-contain lg:w-auto
       ss:w-full" src={blog.BlogImage} alt={blog.title} />
       <div className="flex flex-col lg:gap-8 lg:p-4 rounded-[10px] ss:items-start ss:gap-3">
-        <h1 className="lg:text-[24px] font-semibold ss:text-[12px] ss:leading-5">{blog.title}</h1>
-        <p className="lg:text-[15px] ss:text-[10px] text-gray-500">{blog.description}</p>
+        <h1 className="lg:text-[24px] font-semibold ss:text-[12px] ss:leading-5 sm:text-[20px] sm:leading-7">{blog.title}</h1>
+        <p className="lg:text-[15px] ss:text-[10px] sm:text-[16px] sm:leading-7 text-gray-500">{blog.description}</p>
 
         <div className="flex lg:flex-row lg:items-center lg:gap-12 ss:flex-col ss:items-start ss:gap-6">
           <div className="flex items-center lg:gap-4 ss:gap-2">
@@ -15,7 +15,7 @@ const BlogCard = ({ blog }) => {
             <p className="font-bold whitespace-nowrap ss:text-xs sm:text-base">{blog.username}</p>
           </div>
           <div className="h-8 w-[1px] bg-gray-400 ss:hidden lg:block"></div>
-          <p className="text-gray-400 ss:text-[8px] lg:text-[12px]">{blog.date}</p>
+          <p className="text-gray-400 ss:text-[8px] lg:text-[12px] sm:text-[12px]">{blog.date}</p>
         </div>
       </div>
     </div>
